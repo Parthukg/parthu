8583133af0a12e33efddf2a971e8efecd8fb6143
Parthasarathy KG <kkfp693@astrazeneca.net>
2025-01-01 00:00:15 +0530

    Parthu Portfolio - Commit 25 - Updated profile, projects and blogs fetch url

--- src/containers/projects/Projects.js
+++ src/containers/projects/Projects.js
@@ -16,7 +16,7 @@ export default function Projects() {
 
   useEffect(() => {
     const getRepoData = () => {
-      fetch("parthu/profile.json")
+      fetch("/profile.json")
         .then(result => {
           if (result.ok) {
             return result.json();
